7e31aa601f1e25d9ffd3d3e2d3b04bc3bb63a3c5
Irish Soriano (Student) <N00240541@iadt.ie>
2025-09-16 16:13:41 +0100

    Intial commit

---
+++ my-plant-app/routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PlantController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/plants', [PlantController::class, 'index']);
