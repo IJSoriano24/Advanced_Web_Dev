01fde2e038583b2d4a2347af49606073db893bce
Irish Soriano (Student) <N00240541@iadt.ie>
2025-09-29 14:33:01 +0100

    edits

---
+++ 2025-Dragon-Review/app/Http/Controllers/DragonController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Dragon;
+use Illuminate\Http\Request;
+
+class DragonController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Dragon $dragon)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Dragon $dragon)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Dragon $dragon)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Dragon $dragon)
+    {
+        //
+    }
+}
---
+++ 2025-Dragon-Review/database/migrations/2025_09_29_124434_create_dragons_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('dragons', function (Blueprint $table) {
+            $table->id();
+            $table->string('type');
+            $table->string('color');
+            $table->string('personality');
+            $table->('image');
+            $table->timestamps('created_at');
+            $table->timestamps('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('dragons');
+    }
+};
