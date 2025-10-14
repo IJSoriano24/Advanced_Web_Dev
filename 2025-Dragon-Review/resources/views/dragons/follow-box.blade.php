decb287eadfb6912da9ed0d96c46000803ca8cd0
Irish Joy Soriano <irishjoysoriano@Irishs-MacBook-Air.local>
2025-10-14 22:25:21 +0100

    attempt at adding a search bar

---
+++ 2025-Dragon-Review/resources/views/dragons/follow-box.blade.php
@@ -0,0 +1,33 @@
+<div class="card mt-3">
+    <div class="card-header pb-0 border-0">
+        <h5 class="">Riders</h5>
+    </div>
+
+    <div class="card-body">
+        <div class="hstack gap-2 mb-3">
+            <div class="avatar">
+                <a href="#!"><img class="avatar-img rounded circle"
+                    src="{{ asset('images/dragons/dragonlogo.png') }}" alt="">
+                </a>
+            </div>
+            <div class="overflow-hidden">
+                <a class="h6 mb-0" href="#!">Astrid Hofferson</a>
+                <p class="mb-0 small text-truncate">@Astrid</p>
+            </div>
+            <a class="btn btn-primary-soft rounded-circle icon-md ms-auto" href="#"><i class="fa-solid fa-plus"></i></a>
+        </div>
+        <div class="hstack gap-2 mb-3">
+            <div class="avatar">
+                <a href="#!"><img class="avatar-img rounded circle"
+                    src="{{ asset('images/dragons/dragonlogo.png') }}" alt="">
+                </a>
+            </div>
+            <div class="overflow-hidden">
+                <a class="h6 mb-0" href="#!">Hiccup Haddock</a>
+                <p class="mb-0 small text-truncate">@Hiccup</p>
+            </div>
+            <div class="d-grid mt-3">
+                <a class="btn btn-primary-soft" href="#!">Show More</a>
+            </div>
+        </div>
+    </div>
