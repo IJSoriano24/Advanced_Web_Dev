2a185ed480e1676429dea9db61d4e48c0161f15a
Irish Joy <IJSoriano305@gmail.com>
2025-11-09 17:53:01 +0000

    test

--- 2025-Dragon-Review/resources/views/dragons/show.blade.php
+++ 2025-Dragon-Review/resources/views/dragons/show.blade.php
@@ -16,6 +16,9 @@
 
 {{-- adds a blurred backdrop --}}
     <div class="relative bg-white/60 backdrop-blur max-w-7xl mx-auto  lg:px-8 rounded-lg shadow-sm  py-5">
+
+
+
         <div class="p-6 text-gray-900">
             <h3 class="font-semibold text-lg mb-4">Dragon Details</h3>
             <x-dragon-details
@@ -43,24 +46,27 @@
         @endif
 
         {{-- add a new ability --}}
-        <h4 class="font-semibold text-md mt-8">Add New Ability</h4>
-        <form action="{{ route('abilities.store', $dragon) }}" method="POST" class="mt-4">
-            @csrf
-           <div class="mb-4">
+        @if(auth()->user()->role === 'admin')
+            <h4 class="font-semibold text-md mt-8">Add New Ability</h4>
+            <form action="{{ route('abilities.store', $dragon) }}" method="POST" class="mt-4">
+                @csrf
+
+            <div class="mb-4">
+                    <label for="description" class="block font-medium text-sm text-gray-700">Ability Name:</label>
+                    <textarea name="description" id="description" rows="3" class="mt-1 block w-full" placeholder="Write the name here..."></textarea>
+            </div>
+
+            <div class="mb-4">
                 <label for="name" class="block font-medium text-sm text-gray-700">Description:</label>
-                <input type="text" name="name" id="name" rows="3" class="mt-1 block w-full rounded border-gray-300 shadow-sm" placeholder="Write the ability here..."></input>
-           </div>
+                <input type="text" name="name" id="name" rows="3" class="mt-1 block w-full rounded border-gray-300 shadow-sm" placeholder="Write the description of the ability here..."></input>
+            </div>
 
-           <div class="mb-4">
-                <label for="description" class="block font-medium text-sm text-gray-700">Ability Name:</label>
-                <textarea name="description" id="description" rows="3" class="mt-1 block w-full" placeholder="Write the description here..."></textarea>
-           </div>
-
-           <button type="submit" class="bg-blue-600 text-white hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                Submit Ability
-           </button>
-        </form>    
-
+            <button type="submit" class="bg-blue-600 text-white hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    Submit Ability
+            </button>
+            </form>
+         @endif 
+    </div>
             {{-- displays a youtube video for the dragon based on the "video_id" --}}
         <iframe 
             class="relative z-10 mx-auto block rounded-xl shadow-lg"
